52f768d4a94194abb19fd8ea1f162aacb675e959
Paulus_Makinta <paulusmakinta@icloud.com>
2019-06-03 10:13:37 +0200

    Customized the submit button

--- src/app/orders/order-items/order-items.component.ts
+++ src/app/orders/order-items/order-items.component.ts
@@ -3,6 +3,8 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
 import { OrderItem } from 'src/app/shared/order-item.model';
 import { ItemService } from 'src/app/shared/item.service';
 import { Item } from 'src/app/shared/item.model';
+import { NgForm } from '@angular/forms';
+import { OrderService } from 'src/app/shared/order.service';
 
 @Component({
   selector: 'app-order-items',
@@ -12,14 +14,18 @@ import { Item } from 'src/app/shared/item.model';
 export class OrderItemsComponent implements OnInit {
   formData: OrderItem;
   itemList: Item[];
+  // tslint:disable-next-line:no-inferrable-types
+  isValid: boolean = true;
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data,
     public dialogRef: MatDialogRef<OrderItemsComponent>,
-    private itemService: ItemService) { }
+    private itemService: ItemService,
+    private orderService: OrderService) {}
 
   ngOnInit() {
     this.itemService.getItemList().then(res => this.itemList = res as Item[]);
+    if (this.data.orderItemIndex == null) {
     this.formData = {
       OrderItemId: null,
       OrderId: this.data.OrderId,
@@ -29,6 +35,49 @@ export class OrderItemsComponent implements OnInit {
       Quantity: 0,
       Total: 0
     };
+  } else {
+    this.formData = Object.assign({}, this.orderService.orderItems[this.data.orderItemIndex]);
+  }
   }
 
+  updatePrice(ctrl) {
+// tslint:disable-next-line:triple-equals
+if (ctrl.selectedIndex == 0) {
+   this.formData.Price = 0;
+   this.formData.ItemName = '';
+
+} else {
+  this.formData.Price = this.itemList[ctrl.selectedIndex - 1].Price;
+  this.formData.ItemName = this.itemList[ctrl.selectedIndex - 1].Name;
+   }
+this.updateTotal();
+  }
+
+updateTotal() {
+  this.formData.Total = parseFloat((this.formData.Price * this.formData.Quantity).toFixed(2));
+}
+onSubmit(form: NgForm) {
+  if (this.validateForm(form.value)) {
+    if (this.data.orderItemIndex == null) {
+this.orderService.orderItems.push(form.value);
+
+    } else {
+
+      this.orderService.orderItems[this.data.orderItemIndex] = form.value;
+    }
+    this.dialogRef.close();
+}
+}
+validateForm(formData: OrderItem) {
+  this.isValid = true;
+  // tslint:disable-next-line:triple-equals
+  if (formData.ItemId == 0) {
+    this.isValid = false;
+  // tslint:disable-next-line:triple-equals
+  } else if (formData.Quantity == 0) {
+   this.isValid = false;
+  }
+  return this.isValid;
+
+}
 }
--- src/app/shared/item.model.ts
+++ src/app/shared/item.model.ts
@@ -1,4 +1,5 @@
 export class Item {
     ItemId: number;
     Name: string;
+    Price: number;
 }
